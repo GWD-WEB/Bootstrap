aaedea320c9e50e0bc69884ecacec6eb656cebc9
Willen Goulart <willen@gwd-web.com.br>
2015-05-26 17:38:49 -0300

    Commit inicial

---
+++ src/View/Helper/BsHelper.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace Bootstrap\View\Helper;
+
+use Cake\View\Helper;
+
+class BsHelper extends Helper{
+	
+	var $helpers = ['Html', 'Flash'];
+	private $tooltip_loaded = false;
+	
+	public function loadTooltip(){
+		$this->tooltip_loaded = true;
+		echo "jQuery(function(){\n $('[data-toggle=\"tooltip\"]').tooltip();\n });";
+	}
+	
+	public function tooltipNotLoaded(){
+		echo "<br><div class='alert alert-warning'>".__('Você está usando Bootstrap ToolTip. Por favor, utilize a função $this->Bs->loadTooltip() dentro de tags script')."</div>";
+	}
+	
+	public function button($text, $link, $options){
+		if(isset($options['type'])){
+			$html['class'] = 'btn btn-'.$options['type'];
+		}
+		else $html['class'] = 'btn btn-default';
+		
+		if(isset($options['tooltip'])){
+			if(!$this->tooltip_loaded) $this->tooltipNotLoaded();
+			$html['data-toggle'] = "tooltip";
+			$html['data-placement'] = "top";
+			$html['title'] = $options['tooltip'];
+		}
+		
+		if(isset($options['glyphicon'])){
+			$glyphicon = '<span class="glyphicon glyphicon-'.$options['glyphicon'].'"></span>';
+			if($text != '') $text = $glyphicon.' '.$text;
+			else $text = $glyphicon;
+			$html['escape'] = false;
+		}
+		
+		if(isset($options['class'])){
+			$html['class'] .= ' '.$options['class'];
+		}
+		echo $this->Html->link($text, $link, $html);
+	}
+	
+}
